9f8a5fd221d6500b7eb6c6fa8a767bc47a65ad4e
shubham-369 <shubham.srivastav666@gmail.com>
2023-11-28 15:38:15 +0530

    counterup working

--- src/components/parallax.js
+++ src/components/parallax.js
@@ -1,53 +1,64 @@
-import React from 'react'
-import '../css/parallax.css'
+import React, { useState } from "react";
+import "../css/parallax.css";
+import CountUp from "react-countup";
+import ScrollTrigger from "react-scroll-trigger";
 
 const Parallax = ({ Page }) => {
 
+  const [countStart, setCountStart] = useState(false);
+
   const counter = [
-    {title: 'COMPLETED PROJECTS', data: 3000},
-    {title: 'HAPPY CLIENTS', data: 320},
-    {title: 'CUP OF COFFEE', data: 1000},
-    {title: 'ENGINEERS & STAFF', data: 587},
-  ]
+    { title: "COMPLETED PROJECTS", data: 3000 },
+    { title: "HAPPY CLIENTS", data: 320 },
+    { title: "CUP OF COFFEE", data: 1000 },
+    { title: "ENGINEERS & STAFF", data: 587 },
+  ];
 
   return (
     <>
-    {(Page == 'Home')? (
-      <div className="parallax-home">
-        <div className="details">
-          <h1>WE WILL BE HAPPY TO TAKE CARE OF YOUR WORK</h1>
-          <div className="buttons">
-            <a href="/contact">Contact Us</a>
-            <a href="/services">Services</a>
+      {Page == "Home" ? (
+        <div className="parallax-home">
+          <div className="details">
+            <h1>WE WILL BE HAPPY TO TAKE CARE OF YOUR WORK</h1>
+            <div className="buttons">
+              <a href="/contact">Contact Us</a>
+              <a href="/services">Services</a>
+            </div>
           </div>
         </div>
-      </div>
-    ) : (Page == 'Counter')? (
-      <div className="parallax-counter">
-        {
-          counter.map((element, index) => {
-            return (
-              <div className='counter-box' key={index}>
-                  <h1>{element.data}</h1>
-                  <p>{element.title}</p>
-              </div>
-            )
-          })
-        }
-      </div>
-    ):(
-     <div className='parallax-container'>
-        <nav aria-label="breadcrumb">
+      ) : Page == "Counter" ? (
+        <ScrollTrigger onEnter={() => setCountStart(true)} onExit={() => setCountStart(false)}>
+          <div className="parallax-counter">
+            {counter.map((element, index) => {
+              const {data, title} = element;
+              return (
+                <div className="counter-box" key={index}>
+                  <h1>
+                  { countStart && <CountUp start={0} end={data} duration={3} delay={0}/> }
+                  </h1>
+                  <p>{title}</p>
+                </div>
+              );
+            })}
+          </div>
+        </ScrollTrigger>
+      ) : (
+        <div className="parallax-container">
+          <nav aria-label="breadcrumb">
             <ol className="breadcrumb">
-                <li className="breadcrumb-item"><a href="/">Home</a></li>
-                <li className="breadcrumb-item active" aria-current="page">{Page}</li>
+              <li className="breadcrumb-item">
+                <a href="/">Home</a>
+              </li>
+              <li className="breadcrumb-item active" aria-current="page">
+                {Page}
+              </li>
             </ol>
-        </nav>
-        <h1>{ Page }</h1>
-     </div> 
-    )}
+          </nav>
+          <h1>{Page}</h1>
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Parallax
+export default Parallax;
